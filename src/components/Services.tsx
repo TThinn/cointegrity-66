f5ff002cef7cca45a50c603977146e711e101665
Torstein <51179147+TThinn@users.noreply.github.com>
2025-04-15 00:14:14 +0200

    Update Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -97,8 +97,8 @@ const Services = () => {
           ))}
         </div>
 
-        <div className="text-center max-w-3xl mx-auto mb-16">
-          <h3 className="mt-2 text-3xl md:text-4xl font-bold tracking-tight text-gray-800">Bridging Traditional Business and Decentralized Innovation</h3>
+        <div className="mt-4 text-center max-w-3xl mx-auto mb-16">
+          <h3 className="mt-2 text-3xl md:text-4xl font-bold tracking-tight text-gray-800">Empowering Businesses in a Decentralized World</h3>
           <p className="mt-4 text-lg text-gray-600">
             We're a holistic advisory group, leveraging decades of experience to deliver strategic planning, tokenomics design, regulatory navigation, and implementation support to drive blockchain projects from initial concept to market leadership. Our aim is to bridge the gap between established traditional business and the emerging decentralized landscape. We focus on customer-centric services that deliver exceptional value.
           </p>
