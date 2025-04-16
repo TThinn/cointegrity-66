3bc4fb90c6047c60c59f6a491dcbfd3f2dda047d
Torstein <51179147+TThinn@users.noreply.github.com>
2025-04-16 14:51:08 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,8 +12,8 @@ const Hero = () => {
   const particles = useRef(
     Array.from({ length: HERO_PARTICLE_COUNT_DESKTOP }, () => ({
       size: Math.random() * 800,
-      x: 10 + Math.random() * 90,
-      y: 10 + Math.random() * 90,
+      x: 10 + Math.random() * 70,
+      y: 10 + Math.random() * 70,
       moveX: (Math.random()) * 100,
       moveY: (Math.random()) * 100,
       rotate: Math.random() * 360,
