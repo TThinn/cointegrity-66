b17de4fbc4a869b26dca8b70003103200466a871
Torstein <51179147+TThinn@users.noreply.github.com>
2025-04-16 15:10:21 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -77,7 +77,7 @@ const Hero = () => {
           </h1>
 
           <h2
-            className="text-lg max-w-2xl text-balance animate-fade-up relative mb-16 text-white/80"
+            className="text-lg max-w-2xl text-balance animate-fade-up relative mb-8 text-white/80"
             style={{ animationDelay: "0.4s" }}
           >
             We provide Full-cycle Web3 Consulting Solutions at the Intersection of Blockchain, Ai, Gaming, Tax & Compliance.
