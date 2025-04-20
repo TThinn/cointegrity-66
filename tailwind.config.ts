e25ceae8be9367294303ecc2143a5323c9f80312
Torstein <51179147+TThinn@users.noreply.github.com>
2025-04-20 13:23:28 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,19 +75,7 @@ export default {
 					900: "#1E3A8A",
 					950: "#172554",
 				},
-				"purple": {
-					50: "#F5F3FF",
-					100: "#EDE9FE",
-					200: "#DDD6FE",
-					300: "#C4B5FD",
-					400: "#A78BFA",
-					500: "#8B5CF6",
-					600: "#7C3AED",
-					700: "#6D28D9",
-					800: "#5B21B6",
-					900: "#4C1D95",
-					950: "#2E1065",
-				},
+				// "purple" block has been removed
 			},
 			borderRadius: {
 				lg: 'var(--radius)',
