e8e7a6fb9486b8bb2ba4012ca1d2bea9224c256b
Torstein <51179147+TThinn@users.noreply.github.com>
2025-04-25 10:20:01 +0200

    Update Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -3,25 +3,25 @@ import Container from "./ui/Container";
 import { Target, Layers, Scale, Rocket } from "lucide-react";
 import Button from "./ui/CustomButtonComponent";
 const services = [{
-  icon: <Target size={24} className="text-[#133a63]" />,
+  icon: <Target size={24} className="text-[#3b7698]" />,
   title: "STRATEGIC POSITIONING",
   items: ["Product Market Fit", "Go-To-Market", "Messaging", "Partnerships", "Ecosystem Mapping", "Market Insight Report", "Advisory Board"],
-  color: "blue"
+  color: "[#3b7698]"
 }, {
   icon: <Layers size={24} className="text-[#cb46b3]" />,
   title: "WEB3 INFRASTRUCTURE",
   items: ["Tokenomics", "Smart Contracts", "Web3 Development", "Community Development", "Exchange Listings", "Whitepaper", "Ecosystem Audit"],
-  color: "indigo"
+  color: "[#cb46b3]"
 }, {
-  icon: <Scale size={24} className="text-[#133a63]" />,
+  icon: <Scale size={24} className="text-[#3b7698]" />,
   title: "REGULATORY NAVIGATION",
   items: ["MiCA", "Crypto Tax Returns", "Tax & Legal Mapping", "Banking, Accounting & Auditing", "Due Diligence Reports", "Crypto Compliance", "AML/KYC/CFT"],
-  color: "blue"
+  color: "[#3b7698]"
 }, {
   icon: <Rocket size={24} className="text-[#cb46b3]" />,
   title: "CAPITAL ACCELERATION",
   items: ["Investor Network Access", "Investor Relations Support", "Market Making", "Grants", "Pitch Coaching", "Collateral", "Data Room"],
-  color: "indigo"
+  color: "[#cb46b3]"
 }];
 const Services = () => {
   return <section id="services" className="py-20 bg-gradient-to-b from-[#fbf9ff] to-[#fdf5fa] relative">
